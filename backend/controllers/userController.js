a32683767e1947b8e5d6ef2d42bcb50cf919bb6e
charl321 <65579981+charl321@users.noreply.github.com>
2022-03-14 16:07:51 +0800

    users controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,12 @@
+
+const registerUser = (req, res) => {
+    res.json({ message: 'Register User' })
+}
+
+const loginUser = (req, res) => {
+    res.json({ message: 'Login User' })
+}
+
+module.exports = {
+    registerUser
+}
